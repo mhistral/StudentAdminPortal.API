5ef2e0b335f3f4e63b9ac222c70e863cec675804
Kimmy <kimberlyparrenas@gmail.com>
2022-07-06 20:18:07 +0800

    Adding Connection to the databse

--- StudentAdminPortal.API/StudentAdminPortal.API/DataModels/StudentAdminContext.cs
+++ StudentAdminPortal.API/StudentAdminPortal.API/DataModels/StudentAdminContext.cs
@@ -1,13 +1,18 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace StudentAdminPortal.API.DataModels
 {
     public class StudentAdminContext : DbContext
     {
+        // constructor
+        public StudentAdminContext(DbContextOptions<StudentAdminContext> option) : base (option)
+        {
 
+        }
+
+        // student table create inside SQL Server when migrations are run
+        public DbSet<Student> Student { get; set; }
+        public DbSet<Gender> Gender { get; set; }
+        public DbSet<Address> Address { get; set; }
     }
 }
